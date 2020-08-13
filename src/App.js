a2c1d1a58ac252eae6208739e5ba12ff4962d04e
Shakila Marando <marandoshakila1@gmail.com>
2020-08-13 15:44:30 -0700

    adding react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import emojipedia from "./libs/emojipedia";
+import Card from "./components/card";
+
+import "./App.css";
+
+function createCard(thaemoji) {
+  return (
+    <Card
+      key={thaemoji.id}
+      emoji={thaemoji.emoji}
+      name={thaemoji.name}
+      meaning={thaemoji.meaning}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((emoji) => createCard(emoji))}
+      </dl>
     </div>
   );
 }
