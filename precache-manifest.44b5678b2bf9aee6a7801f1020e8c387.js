7dbc23fe0d62ce45def9aa915281c988057e3e9d
Shakila Marando <marandoshakila1@gmail.com>
2020-08-13 15:43:59 -0700

    Updates

---
+++ precache-manifest.44b5678b2bf9aee6a7801f1020e8c387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9fa25c872241123286307b2fcd387a",
+    "url": "/reactEmojipedia/index.html"
+  },
+  {
+    "revision": "68ccb097a76c4cd1fdcc",
+    "url": "/reactEmojipedia/static/css/main.5d5dfed5.chunk.css"
+  },
+  {
+    "revision": "7c4d0423e297829c684a",
+    "url": "/reactEmojipedia/static/js/2.dfcac51a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactEmojipedia/static/js/2.dfcac51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ccb097a76c4cd1fdcc",
+    "url": "/reactEmojipedia/static/js/main.52712f63.chunk.js"
+  },
+  {
+    "revision": "e19879eacfcb60293be2",
+    "url": "/reactEmojipedia/static/js/runtime-main.a9fe3e87.js"
+  }
+]);
